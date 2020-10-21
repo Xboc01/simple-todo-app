fa93feb82afba4301d723f778544e3111ea33115
Xboc01 <xavier.boccadoro@gmail.com>
2020-10-21 12:22:36 -0600

    Updates

---
+++ precache-manifest.9d2a87c34b62dcab0b8cbb33402ec790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91557fdadc5a3c72611db8ea032c5c22",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "ea705159e9b78b061600",
+    "url": "/simple-todo-app/static/css/main.7225289e.chunk.css"
+  },
+  {
+    "revision": "a5dd8cd2497efb7d708c",
+    "url": "/simple-todo-app/static/js/2.6ceb921b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-todo-app/static/js/2.6ceb921b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea705159e9b78b061600",
+    "url": "/simple-todo-app/static/js/main.37c83c08.chunk.js"
+  },
+  {
+    "revision": "c5e31f49d9067987ae5a",
+    "url": "/simple-todo-app/static/js/runtime-main.c8feee53.js"
+  }
+]);
